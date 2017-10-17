d495992d9e0573093eff2906a210c245bf406bc6
Ulrich Schmidt <admin@deruli.de>
2017-10-17 21:29:09 +0200

    unit test geschrieben

---
+++ src/content/modules/browser_default_language/tests/BrowserDefaultLanguageTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class BrowserDefaultLanguageTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testParseDefaultLanguage()
+    {
+        $this->assertEquals("de", parseDefaultLanguage("de,en;q=0.8,de-DE;q=0.6,en-US;q=0.4"));
+        $this->assertEquals("fr", parseDefaultLanguage("fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5"));
+    }
+}
